3492ede81601f7b744738ffea3bd57a1bf0fadd7
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-12-09 10:53:58 +0530

    project section

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,31 +1,38 @@
-import pro2 from "../assets/project2.jpg"
-import pro1 from "../assets/project111.jpg"
-import pro3 from "../assets/project33.jpg"
+import pro2 from "../assets/project2.jpg";
+import pro1 from "../assets/project111.jpg";
+import pro3 from "../assets/project33.jpg";
 
+const ProjectCardData = [
+  {
+    imgsrc: pro2,
+    title: "Ecommerce Website Design",
+    text: "Thriving Facebook community and top-ranked podcast, amy inspires a grounded, tanginle and self-affirming sense of wow! I really can do this for over 100,000 online entrepreneures. Best-selling marketing courese",
+    view: "https://www.youtube.com/watch?v=0h2b4ftbZcU",
+  },
+  {
+    imgsrc: pro1,
+    title: "LMS/Course Website Design",
+    text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More.",
+    view: "https://www.youtube.com/watch?v=0h2b4ftbZcU",
+  },
+  {
+    imgsrc: pro3,
+    title: "Rymo Ecommerce Website Design",
+    text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More.",
+    view: "https://www.youtube.com/watch?v=0h2b4ftbZcU",
+  },
+  {
+    imgsrc: pro1,
+    title: "LMS/Course Website Design",
+    text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More.",
+    view: "https://www.youtube.com/watch?v=0h2b4ftbZcU",
+  },
+  {
+    imgsrc: pro3,
+    title: "Rymo Ecommerce Website Design",
+    text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More.",
+    view: "https://www.youtube.com/watch?v=0h2b4ftbZcU",
+  },
+];
 
- 
-const ProjectCardData =[
-    {
-        imgsrc:pro2,
-        title:"Ecommerce Website Design",
-        text: "Thriving Facebook community and top-ranked podcast, amy inspires a grounded, tanginle and self-affirming sense of wow! I really can do this for over 100,000 online entrepreneures. Best-selling marketing courese" ,
-    view:"https://www.youtube.com/watch?v=0h2b4ftbZcU"
-    
-    },
-    {
-        imgsrc:pro1,
-        title:"LMS/Course Website Design",
-        text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More." ,
-    view:"https://www.youtube.com/watch?v=0h2b4ftbZcU"
-    
-    },
-    {
-        imgsrc:pro3,
-        title:"Rymo Ecommerce Website Design",
-        text: "On-Demand Learning with World-Class Global Instructors. Get Started with Udemy Business™. Tailor-Made Programs to Align with Your Company's Goals through eLearning. Learn More. Drive Employee Engagement. Prepare Your Workforce. Empower Your Team. Prove the ROI of Learning. Courses: Business, Tech, Design, and More." ,
-    view:"https://www.youtube.com/watch?v=0h2b4ftbZcU"
-    
-    }
-]
-
-export default ProjectCardData
+export default ProjectCardData;
